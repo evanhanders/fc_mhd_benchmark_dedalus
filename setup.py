9e504d0b22918c84d7c00f5e5454c2fcca41ed39
Evan Anders <evanhanders@gmail.com>
2020-06-05 15:54:05 -0600

    sets up module more effectively

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fce_dedalus',
+      version='0.0.1',
+      description='Tools and scripts for studying fully compressible systems in dedalus',
+      url='https://github.com/evanhanders/fce_dedalus',
+      author='Evan Anders',
+      author_email='evan.anders@colorado.edu',
+      license='GPL-3.0',
+      packages=['fce_dedalus', 'fce_dedalus/logic', 'fce_dedalus/atmospheres'],
+      zip_safe=False)
